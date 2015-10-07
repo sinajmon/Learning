98a4647b647c1571ff843c74c0685037f10ee9c9
sinajmon.ka <sinajmon.ka@spanservice.com>
2015-10-07 18:03:10 +0530

    WebAppForAWS Project

---
+++ WebAppForAWS/WebAppForAWS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppForAWS.Startup))]
+namespace WebAppForAWS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
